bf04605c04ea1a0f9c02611c237259f45fa65f1c
RomaV78 <romav5166@gmail.com>
2022-03-22 21:37:07 +0100

    Completed 50

--- src/com/Main.java
+++ src/com/Main.java
@@ -32,42 +32,86 @@ public class Main {
         System.out.println("\nAnswer: "+sum);
     }
 
-//    /**
-//     * Найти:
-//     *  а) сумму кубов всех целых чисел от 20 до 40;
-//     *  б) сумму кубов всех целых чисел от а до 50 (значение а вводится с клавиатуры; 0 ≤ a ≤ 50);
-//     *  в) сумму кубов всех целых чисел от 1 до n (значение n вводится с клавиатуры; 1 ≤ n ≤ 100);
-//     *  г) сумму кубов всех целых чисел от а до b (значения а и b вводятся с клавиатуры; b ≥ а).
-//     */
-//    private static void task50(){
-//        System.out.println("\n\nTask 50:\nНайти:\n"+
-//                "\tа) сумму кубов всех целых чисел от 20 до 40;\n"+
-//                "\tб) сумму кубов всех целых чисел от а до 50 (значение а вводится с клавиатуры; 0 ≤ a ≤ 50);\n"+
-//                "\tв) сумму кубов всех целых чисел от 1 до n (значение n вводится с клавиатуры; 1 ≤ n ≤ 100);\n"+
-//                "\tг) сумму кубов всех целых чисел от а до b (значения а и b вводятся с клавиатуры; b ≥ а).");
-//        Random random = new Random();
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("\nA)");
-//        double sum=0;
-//
-//        for(int i=20; i<=40; i++){
-//            sum=sum+Math.pow(i,3);
-//        }
-//        System.out.println("Answer: "+sum);
-//
-//
-//        System.out.println("\nБ)");
-//        sum=0;
-//
-//        System.out.print("Enter A range(0,50): ");
-//        int a = scanner.nextInt();
-//
-//        if(a>=0&&a<=50){
-//
-//        }
-//
-//    }
+    /**
+     * Найти:
+     *  а) сумму кубов всех целых чисел от 20 до 40;
+     *  б) сумму кубов всех целых чисел от а до 50 (значение а вводится с клавиатуры; 0 ≤ a ≤ 50);
+     *  в) сумму кубов всех целых чисел от 1 до n (значение n вводится с клавиатуры; 1 ≤ n ≤ 100);
+     *  г) сумму кубов всех целых чисел от а до b (значения а и b вводятся с клавиатуры; b ≥ а).
+     */
+    private static void task50(){
+        System.out.println("\nTask 50:\nНайти:\n"+
+                "\tа) сумму кубов всех целых чисел от 20 до 40;\n"+
+                "\tб) сумму кубов всех целых чисел от а до 50 (значение а вводится с клавиатуры; 0 ≤ a ≤ 50);\n"+
+                "\tв) сумму кубов всех целых чисел от 1 до n (значение n вводится с клавиатуры; 1 ≤ n ≤ 100);\n"+
+                "\tг) сумму кубов всех целых чисел от а до b (значения а и b вводятся с клавиатуры; b ≥ а).");
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nA)");
+        double sum=0;
+
+        for(int i=20; i<=40; i++){
+            sum=sum+Math.pow(i,3);
+        }
+        System.out.println("Answer: "+sum);
+
+
+        System.out.println("\nБ)");
+        sum=0;
+        int a;
+
+        do {
+            System.out.print("Enter A range(0,50): ");
+            a = scanner.nextInt();
+        }
+        while(a<0||a>50);
+
+        while (a<=50){
+            sum=sum+Math.pow(a,3);
+            a++;
+        }
+        System.out.println("\nAnswer: "+sum);
+
+
+        System.out.println("\nВ)");
+        sum=0;
+        int n;
+
+        do {
+            System.out.print("Enter N range(1,100): ");
+            n = scanner.nextInt();
+        }
+        while(n<1||n>100);
+
+        while (n<=100){
+            sum=sum+Math.pow(n,3);
+            n++;
+        }
+        System.out.println("\nAnswer: "+sum);
+
+
+        System.out.println("\nГ)");
+        sum=0;
+        a=0;
+        int b=0;
+
+        do {
+            System.out.print("\nB>=A\nEnter A: ");
+            a = scanner.nextInt();
+            System.out.print("Enter B: ");
+            b = scanner.nextInt();
+
+        }
+        while(a>b);
+
+        while (a<=b){
+            sum=sum+Math.pow(a,3);
+            a++;
+        }
+        System.out.println("\nAnswer: "+sum);
+
+    }
 
     /**
      * Вычислить сумму: √(1+√(1+√(3+...+√(50))))..
@@ -80,6 +124,6 @@ public class Main {
     public static void main(String[] args) {
 	    Start();
         task25();
-        //task50();
+        task50();
     }
 }
